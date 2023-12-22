233b346bf38852776f52e7ed694853f6acdbd60a
NikhilBadole <120246851+NikhilBadole@users.noreply.github.com>
2023-12-22 14:52:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 //your JS code here. If required.
-const btn = document.getElementById("enterBtn");
-btn.addEventListener("Click",changeName());
 
-function changeName(){
-	const pTag = document.getElementById("status");
-	pTag.textContent = "Entered Metaverse";
-	var h1Element = document.createElement('h1');
-	h1Element.textContent = "Entered Metaverse";
-	pTag.parentNode.replaceChild(h1Element, pTag);
-}
+function updateText() {
+      // Get the p tag with id 'status'
+      var statusParagraph = document.getElementById('status');
+
+      // Change the text content of the p tag
+      statusParagraph.textContent = "Entered Metaverse";
+
+      // Create a new h1 element
+      var h1Element = document.createElement('h1');
+      // Set its text content
+      h1Element.textContent = "Entered Metaverse";
+
+      // Replace the existing p tag with the new h1 tag
+      statusParagraph.parentNode.replaceChild(h1Element, statusParagraph);
+    }

b090eb0721ee3a391071f77a9bb210809e2d70eb
NikhilBadole <120246851+NikhilBadole@users.noreply.github.com>
2023-12-22 14:49:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const btn = document.getElementById("enterBtn");
+btn.addEventListener("Click",changeName());
+
+function changeName(){
+	const pTag = document.getElementById("status");
+	pTag.textContent = "Entered Metaverse";
+	var h1Element = document.createElement('h1');
+	h1Element.textContent = "Entered Metaverse";
+	pTag.parentNode.replaceChild(h1Element, pTag);
+}
